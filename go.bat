01cb40f8db96dffef2c1dda353b4ea6e77a61b4d
David Schmidt <1110325+david-schmidt@users.noreply.github.com>
2023-09-08 14:25:31 -0400

    Prepare both sizes of disk images (#7)
    * Add 800k disk image
    
    Signed-off-by: david-schmidt <1110325+david-schmidt@users.noreply.github.com>
    
    * Remove stale GlobalConst.s file
    
    Signed-off-by: david-schmidt <1110325+david-schmidt@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: david-schmidt <1110325+david-schmidt@users.noreply.github.com>

--- go.bat
+++ go.bat
@@ -13,6 +13,8 @@ ca65 --cpu 65C02 DiskMaker8.s -l ..\DiskMaker8.lst
 ld65 -o ..\DiskMaker8 DiskMaker8.o -m ..\DiskMaker8.map -C DiskMaker8.cfg
 cd ..
 
-@REM Put it on a disk image
-copy DiskMaker8Base.dsk DM8.dsk
-type DiskMaker8 | ac -p DM8.dsk DiskMaker8.SYS SYS 0x2000
+@REM Put it on disk images
+copy DiskMaker8Base.dsk DiskMaker8.dsk
+copy DiskMaker8Base.po DiskMaker8.po
+type DiskMaker8 | ac -p DiskMaker8.dsk DiskMaker8 SYS 0x2000
+type DiskMaker8 | ac -p DiskMaker8.po DiskMaker8 SYS 0x2000
